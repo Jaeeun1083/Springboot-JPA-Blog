1e8aa994258b61e8e4ca254d14dc1bd88ccf6e4a
jaeeun <jjomang7602@naver.com>
2021-03-23 18:17:21 +0900

    update 테스트 (더티체킹)

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -44,6 +48,34 @@ public class DummyControllerTest {
 	//의존성 주입.(DI)
 	
 	
+	//email,password를 받아야함 ->수정 하려고
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //json데이터 받으려고 @RequestBody. //json데이터를 요청하면 Java Object(MessageConverter의 Jackson라이브러리가 변환해서 받아줌.
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+
+		//userRepository.save(requestUser);
+		return null;
+		
+		//save함수는 id를 전달하지 않으면 insert를 해주고
+		//					 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+		//					 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해준다.
+		//DB에서 user를 들고와서 
+	}
+	
+	
+	
+	
+	
+	
 	//전체 select
 	@GetMapping("/dummy/users")
 	public List<User> list(){
@@ -52,11 +84,11 @@ public class DummyControllerTest {
 	
 	//페이징 List. 한 페이지 당 2건에 데이터를 리턴받아 볼 예정
 	@GetMapping("/dummy/user")
-	public Page<User> pageList(@PageableDefault(size=2, sort="id", direction=Direction.DESC) Pageable pageable){
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Direction.DESC) Pageable pageable){
 		Page<User> pagingUser = userRepository.findAll(pageable);
 		
-		List<User> users = pagingUser.getContent();
-		return paginguser;
+		List<User> users = pagingUser.getContent(); //내용만 리턴하기 위해.
+		return users;
 		
 	}
 	
